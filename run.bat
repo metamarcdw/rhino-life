39df007a617d4f3ad245f7fc12db05fa915e053f
Marc Wood <marcdw87@gmail.com>
2019-12-28 14:14:23 -0500

    Add run scripts

---
+++ run.bat
@@ -0,0 +1,3 @@
+@echo off
+java -cp "rhino-1.7.11.jar" org.mozilla.javascript.tools.shell.Main index.js
+pause
---
+++ run.sh
@@ -0,0 +1,1 @@
+java -cp "rhino-1.7.11.jar" org.mozilla.javascript.tools.shell.Main index.js
